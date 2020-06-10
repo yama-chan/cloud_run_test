73b1b87a7053ef5f8ac913aeba5eec0677467a9f
yama-chan <>
2020-06-10 11:27:45 +0900

    コメント・修正

--- lib/mail/mail.go
+++ lib/mail/mail.go
@@ -19,11 +19,11 @@ const (
 )
 
 type (
-
 	// EmailSender メール送信インターフェース
 	EmailSender interface {
 		SendBroad(ctx context.Context, conf EmailSenderConfig) error
 	}
+
 	// EmailSenderConfig メール送信設定
 	EmailSenderConfig struct {
 		Message   Message
@@ -31,16 +31,17 @@ type (
 		FromEmail string
 		// ※トランザクショナルテンプレートを使用してリクエストにtemplate_IDを指定している場合はContentは必須ではありません。
 		// ContentType EmailContentType
-		// To []EmailAddress
 		To []string
+		// TODO: Goルーチンを使って複数メール送信の際に並列で送信出来るか実装する
 		// SendFailedChan chan<- EmailSendFailed
 	}
 
+	// Message メールの内容
 	Message struct {
-		ID string `validate:"required"` // お知らせID
+		// ID string `validate:"required"` // お知らせID
 		// Type         NotificationType `validate:"required"` // お知らせタイプ
-		Title        string    `validate:"required"` // タイトル
-		Body         string    // 詳細内容
 		RegisteredAt time.Time `validate:"required"` // 作成日時
+		Title        string    `validate:"required"` // タイトル
+		// Body         string    // メール本文 ※テンプレートを使用するので今回はコメント
 	}
 )
--- lib/mail/sendgrid/service.go
+++ lib/mail/sendgrid/service.go
@@ -7,6 +7,7 @@ import (
 	"github.com/taisukeyamashita/test/lib/mail"
 )
 
+// Send メール送信　EmailSenderConfig(メール送信設定)をうけとって送信
 func Send(ctx context.Context, config mail.EmailSenderConfig) error {
 	err := ProvideEmailSender().SendEmail(ctx, config)
 	if err != nil {
